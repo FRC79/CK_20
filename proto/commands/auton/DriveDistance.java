f4829ea259c4dee708250925a3f0c7da98e0e7c5
Zach <zachnigels@gmail.com>
2017-01-21 11:39:30 -0800

    Sping Cleaning (of the code)

---
+++ proto/commands/auton/DriveDistance.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team79.robot.commands.auton;
+
+import org.usfirst.frc.team79.robot.commands.CommandBase;
+
+/**
+ *
+ */
+public class DriveDistance extends CommandBase {
+
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+}
---
+++ proto/robot/commands/CommandBase.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team79.robot.commands;
+
+import org.usfirst.frc.team79.robot.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public abstract class CommandBase extends Command {
+	
+	public static DriveTrain driveTrain;
+	
+	public static void init(){
+		driveTrain = new DriveTrain(); 
+	}
+	
+    public CommandBase() {   
+    	super();
+    }
+
+}
