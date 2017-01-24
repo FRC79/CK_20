0f4a082774794b7e797707d4e4c50f1e52d58f6f
Austin <aj_viens_deruisseau@yahoo.com>
2017-01-23 20:01:18 -0500

    Adds Intake Subsystem
    Added Intake

---
+++ src/commands/stopIntake.java
@@ -0,0 +1,18 @@
+package commands;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class stopIntake extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
