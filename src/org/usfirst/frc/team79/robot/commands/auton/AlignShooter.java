4a9841fac69a9255f5646bcdebc24a463bfcab5d
Noah <xlash19o13@gmail.com>
2017-01-24 19:54:21 -0500

    Fixed how GRIP data is received

--- src/org/usfirst/frc/team79/robot/commands/auton/AlignShooter.java
+++ src/org/usfirst/frc/team79/robot/commands/auton/AlignShooter.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team79.robot.commands.auton;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class AlignShooter extends CommandGroup {
 	
@@ -10,10 +11,9 @@ public class AlignShooter extends CommandGroup {
 	 */
 	public AlignShooter() {}
 	
-	public void execute(){
-		//The NetworkTable used to get GRIP data from drive station. All data will be calculated on driver station.
-		NetworkTable grip = NetworkTable.getTable("grip");
-		addSequential(new RotateDegrees(grip.getNumber("boilerTape/yawDegrees", 0d)));
+	protected void execute(){
+		addSequential(new ProcessGripData());
+		addSequential(new RotateDegrees(SmartDashboard.getNumber("boilerHeader", 0)));
 		//There will probably be more commands, like rotating pitch.
 	}
 
