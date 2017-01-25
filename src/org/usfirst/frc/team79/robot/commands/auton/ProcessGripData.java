7ed3d6ae4eef8ff2d15e97e186a805f115bb2958
3w.jq <3w.jq@S6181LENG149-16.pinellas.local>
2017-01-25 18:01:19 -0500

    Changed parent class and removed Contour class

--- src/org/usfirst/frc/team79/robot/commands/auton/ProcessGripData.java
+++ src/org/usfirst/frc/team79/robot/commands/auton/ProcessGripData.java
@@ -1,25 +1,15 @@
 package org.usfirst.frc.team79.robot.commands.auton;
 
-import org.usfirst.frc.team79.robot.commands.CommandBase;
+import org.usfirst.frc.team79.robot.RobotMap;
+import org.usfirst.frc.team79.robot.grip.Contour;
 
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class ProcessGripData extends CommandBase{
+public class ProcessGripData extends Command{
 	
 	public NetworkTable grip;
-	
-	//Calcualtes FOV and Focal length of camera. May be inaccurate due to possible automatic scaling by GRIP.
-	public static final double DIAGONAL_FOV = 68.5; //degrees
-	public static final double HORIZONTAL_ASPECT_RATIO = 4;
-	public static final double VERTICAL_ASPECT_RATIO = 3;
-	public static final double IMAGE_WIDTH = 640; //pixels
-	public static final double IMAGE_HEIGHT = 480; //pixels
-	public static final double HORIZONTAL_FOV = Math.toDegrees(Math.atan(Math.tan(Math.toRadians(DIAGONAL_FOV/2))*(4/Math.sqrt(Math.pow(HORIZONTAL_ASPECT_RATIO, 2) + Math.pow(VERTICAL_ASPECT_RATIO, 2))))*2);; //degrees
-	public static final double FOCAL_LENGTH = IMAGE_WIDTH / (2*Math.tan(Math.toRadians(HORIZONTAL_FOV/2))); //pixels
-	
-	public static final double CX = IMAGE_WIDTH/2 -.5;
-	public static final double CY = IMAGE_HEIGHT/2 -.5;
 	
 	private boolean finished;
 	
@@ -33,7 +23,7 @@ public class ProcessGripData extends CommandBase{
 	@Override
 	protected void execute(){
 		Contour tape = this.getGreatestContour(getContours());
-		double headerToTarget = Math.toDegrees(Math.atan(Math.toRadians((tape.centerX-CX)/FOCAL_LENGTH)));
+		double headerToTarget = Math.toDegrees(Math.atan(Math.toRadians((tape.centerX-RobotMap.CX)/RobotMap.FOCAL_LENGTH)));
 		//Sends the header to the dashboard to be used in the RotateDegrees command.
 		SmartDashboard.putNumber("boilerHeader", headerToTarget);
 		finished = true;
@@ -73,18 +63,6 @@ public class ProcessGripData extends CommandBase{
 	@Override
 	protected boolean isFinished() {
 		return finished;
-	}
-	
-	public class Contour{
-		
-		public double area, centerX, centerY;
-		
-		public Contour(double area, double centerX, double centerY){
-			this.area = area;
-			this.centerX = centerX;
-			this.centerY = centerY;
-		}
-		
 	}
 
 }
