16c3dc634c85f680b9d110f9d990ccc173083c56
3w.jq <3w.jq@S6181LENG149-16.pinellas.local>
2017-01-28 14:04:12 -0500

    Now activates motors

--- src/org/usfirst/frc/team79/robot/commands/auton/RotateDegrees.java
+++ src/org/usfirst/frc/team79/robot/commands/auton/RotateDegrees.java
@@ -5,18 +5,23 @@ import org.usfirst.frc.team79.robot.commands.CommandBase;
 public class RotateDegrees extends CommandBase {
 
 	double degrees;
+	boolean finished;
 	
-	public RotateDegrees(double degrees) {
+	public RotateDegrees(double degrees){
 		requires(driveTrain);
-		this.degrees = degrees;
+		this.degrees = degrees + driveTrain.getGyroAngle();
 	}
 
 	protected void execute() {
-		//TODO:
+		double displacement = degrees - driveTrain.getGyroAngle();
+		double speed = 0.3d * displacement;
+		driveTrain.FrontLeft.set(speed);
+		driveTrain.FrontRight.set(-speed);
+		finished = Math.abs(displacement) < .2d;
 	}
 
 	protected boolean isFinished() {
-		return false; //TODO:
+		return finished; //TODO:
 	}
 
 	protected void end() {
