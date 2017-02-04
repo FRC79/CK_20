3b7fb0e5efa928714ca6e05ec7015e435962d427
Ashytech10 <Ashytech10@10.162.247.170>
2017-02-04 12:17:52 -0500

    PIDController works

--- src/org/usfirst/frc/team79/robot/commands/auton/RotateDegrees.java
+++ src/org/usfirst/frc/team79/robot/commands/auton/RotateDegrees.java
@@ -2,10 +2,12 @@ package org.usfirst.frc.team79.robot.commands.auton;
 
 import org.usfirst.frc.team79.robot.Robot;
 import org.usfirst.frc.team79.robot.utilities.GyroPIDOutput;
+import org.usfirst.frc.team79.robot.utilities.GyroPIDSource;
 
 import com.ctre.CANTalon.TalonControlMode;
 
 import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -20,18 +22,24 @@ public class RotateDegrees extends Command {
 	 */
 	public RotateDegrees(double degrees){
 		requires(Robot.driveTrain);
+		Robot.driveTrain.gyro.zeroYaw(); //Temporary for testing
 		this.degrees = degrees + Robot.driveTrain.getGyroAngle();
-		gyroPID = new PIDController(.2, 0, .2, Robot.driveTrain.gyro, new GyroPIDOutput());
-	}
+		gyroPID = new PIDController(0, 0, 0, new GyroPIDSource(), new GyroPIDOutput());
+		}
 	
 	protected void initialize(){
-		gyroPID.setAbsoluteTolerance(0.5);
+		System.out.println("Initializing command");
+		gyroPID.setPID(SmartDashboard.getNumber("Turn P", 0), SmartDashboard.getNumber("Turn I", 0), SmartDashboard.getNumber("Turn D", 0));
+		gyroPID.setContinuous();
+		gyroPID.setAbsoluteTolerance(0.9);
 		Robot.driveTrain.FrontLeft.changeControlMode(TalonControlMode.PercentVbus);
 		Robot.driveTrain.FrontRight.changeControlMode(TalonControlMode.PercentVbus);
+		gyroPID.setSetpoint(degrees);
+		gyroPID.enable();
 	}
 
 	protected void execute() {
-		gyroPID.setSetpoint(degrees);
+		System.out.println("Running command");
 	}
 
 	protected boolean isFinished() {
@@ -39,11 +47,13 @@ public class RotateDegrees extends Command {
 	}
 
 	protected void end() {
+		gyroPID.disable();
 		Robot.driveTrain.stop();
 	}
 
 	protected void interrupted() {
-		
+		gyroPID.disable();
+		Robot.driveTrain.stop();
 	}
 
 }
