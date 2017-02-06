dcf9d76b7d1b99ffc19c0faca8a51796d3ba8f4e
orion <orion@10.13.242.0>
2017-02-06 18:48:13 -0500

    Toggles intake instead of only starting

--- src/org/usfirst/frc/team79/robot/commands/StartIntake.java
+++ src/org/usfirst/frc/team79/robot/commands/StartIntake.java
@@ -1,22 +1,25 @@
 package org.usfirst.frc.team79.robot.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc.team79.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class StartIntake extends Command {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-
+	/**
+	 * Toggles the intake
+	 */
     public StartIntake(){
     	requires(Robot.intake);
-
     }
+    
     public void execute() {
-    	Robot.intake.intake.set(1.0);
+    	if(Robot.intake.intake.get() != 0.0){
+    		Robot.intake.intake.set(0.0);
+    	}else Robot.intake.intake.set(1.0);
     }
 
     public boolean isFinished() {
