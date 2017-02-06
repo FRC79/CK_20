1362b8d7aa88b5b61904a01fd59567ad10050532
orion <orion@10.13.242.0>
2017-02-06 18:49:03 -0500

    Toggles the shooter

---
+++ src/org/usfirst/frc/team79/robot/commands/StartShooter.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team79.robot.commands;
+
+import org.usfirst.frc.team79.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class StartShooter extends Command{
+	
+	/**
+	 * Toggles the shooter
+	 */
+	public StartShooter(){
+		requires(Robot.shooter);
+	}
+	
+	public void execute(){
+		if(Robot.shooter.shooterWheel.get() != 0.0){
+			Robot.shooter.shooterWheel.set(0.0);
+		}else Robot.shooter.shooterWheel.set(1.0);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+}
