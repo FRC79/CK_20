8e22e16ca899a2a694e6c31f90a6eb3037b2c8d0
orion <orion@10.13.242.0>
2017-02-06 18:47:19 -0500

    Adds button mappings for various subsystems

--- src/org/usfirst/frc/team79/robot/OI.java
+++ src/org/usfirst/frc/team79/robot/OI.java
@@ -2,7 +2,11 @@ package org.usfirst.frc.team79.robot;
 
 import org.usfirst.frc.team79.robot.commands.DriveDistance;
 import org.usfirst.frc.team79.robot.commands.DriveSpeed;
+import org.usfirst.frc.team79.robot.commands.StartIntake;
+import org.usfirst.frc.team79.robot.commands.StartShooter;
 import org.usfirst.frc.team79.robot.commands.auton.AlignShooter;
+import org.usfirst.frc.team79.robot.commands.feeder.StartConveyer;
+import org.usfirst.frc.team79.robot.commands.feeder.StartFeeder;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
@@ -16,11 +20,17 @@ public class OI {
 	public Joystick throttleStick = new Joystick(0);
 
 	public Button gripButton = new JoystickButton(throttleStick, 5);
-	public Button driveButton = new JoystickButton(throttleStick, 8);
+	public Button intakeButton = new JoystickButton(throttleStick, 6);
+	public Button feederButton = new JoystickButton(throttleStick, 7);
+	public Button conveyerButton = new JoystickButton(throttleStick, 8);
+	public Button shootButton = new JoystickButton(throttleStick, 4);
 	
 	public OI(){
 		this.gripButton.toggleWhenPressed(new AlignShooter());
-		this.driveButton.toggleWhenPressed(new DriveDistance(120));
+		this.intakeButton.toggleWhenPressed(new StartIntake());
+		this.feederButton.toggleWhenPressed(new StartFeeder());
+		this.conveyerButton.toggleWhenPressed(new StartConveyer());
+		this.shootButton.toggleWhenPressed(new StartShooter());
 	}
 	
 	public Joystick getJoystick() {
