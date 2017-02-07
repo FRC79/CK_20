e68ca9543c38e78a1712f03abdc509bfaa165daf
3w.jq <3w.jq@S6181LENG149-16.pinellas.local>
2017-02-06 20:05:08 -0500

    Basic motion profiling foundation.

---
+++ src/org/usfirst/frc/team79/robot/commands/auton/ExecuteMotionProfile.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team79.robot.commands.auton;
+
+import org.usfirst.frc.team79.robot.Robot;
+
+import com.ctre.CANTalon.MotionProfileStatus;
+import com.ctre.CANTalon.TalonControlMode;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ExecuteMotionProfile extends Command{
+	
+	private MotionProfileStatus lStatus = new MotionProfileStatus();
+	private MotionProfileStatus rStatus = new MotionProfileStatus();
+	
+	public ExecuteMotionProfile(){
+		requires(Robot.driveTrain);
+	}
+	
+	protected void initialize(){
+		Robot.driveTrain.FrontLeft.changeControlMode(TalonControlMode.MotionProfile);
+		Robot.driveTrain.FrontRight.changeControlMode(TalonControlMode.MotionProfile);
+		Robot.driveTrain.FrontLeft.set(1);
+		Robot.driveTrain.FrontRight.set(1);
+	}
+	
+	protected void execute(){
+		Robot.driveTrain.FrontLeft.processMotionProfileBuffer();
+		Robot.driveTrain.FrontRight.processMotionProfileBuffer();
+	}
+	
+	protected void end(){
+		Robot.driveTrain.FrontLeft.set(0);
+		Robot.driveTrain.FrontRight.set(0);
+	}
+	
+	@Override
+	protected boolean isFinished() {
+		Robot.driveTrain.FrontLeft.getMotionProfileStatus(lStatus);
+		Robot.driveTrain.FrontRight.getMotionProfileStatus(rStatus);
+		return lStatus.activePoint.isLastPoint && rStatus.activePoint.isLastPoint;
+	}
+
+}
---
+++ src/org/usfirst/frc/team79/robot/commands/auton/GenerateMotionProfile.java
@@ -0,0 +1,52 @@
+package org.usfirst.frc.team79.robot.commands.auton;
+
+import org.usfirst.frc.team79.robot.Robot;
+
+import com.ctre.CANTalon.TrajectoryPoint;
+
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.Waypoint;
+import jaci.pathfinder.Trajectory.Config;
+import jaci.pathfinder.Trajectory.Segment;
+import jaci.pathfinder.modifiers.TankModifier;
+
+public class GenerateMotionProfile{
+	
+	public final static Config config = new Config(Trajectory.FitMethod.HERMITE_CUBIC, Config.SAMPLES_HIGH, 0.05, 1.7, 2.0, 60.0); //TODO Will need tuning
+	
+	/**
+	 * Generates points needed for motion profiling and sends them to the Talon.
+	 * @param points
+	 */
+	public static void generate(Waypoint... points){
+		//TODO wheelbase needed. Distance (meters) between the 2 sides of the drive train.
+		double wheelbase = 2.0;
+		TankModifier modifier = new TankModifier(Pathfinder.generate(points, config)).modify(wheelbase);
+		Trajectory left = modifier.getLeftTrajectory();
+		Trajectory right = modifier.getRightTrajectory();
+		for(int i=0; i<left.segments.length; i++){
+			Segment segment = left.segments[i];
+			TrajectoryPoint point = new TrajectoryPoint();
+			point.position = segment.position;
+			point.velocity = segment.velocity;
+			point.timeDurMs = (int) (segment.dt * 1000); //TODO I assume 'dt' is delta time in seconds. Will check later
+			point.profileSlotSelect = 0;
+			if(i==0) point.zeroPos = true;
+			if(i+1 == left.segments.length) point.isLastPoint = true;
+			Robot.driveTrain.FrontLeft.pushMotionProfileTrajectory(point);
+		}
+		for(int i=0; i<right.segments.length; i++){
+			Segment segment = left.segments[i];
+			TrajectoryPoint point = new TrajectoryPoint();
+			point.position = segment.position;
+			point.velocity = segment.velocity;
+			point.timeDurMs = (int) (segment.dt * 1000); //TODO I assume 'dt' is delta time in seconds. Will check later
+			point.profileSlotSelect = 0;
+			if(i==0) point.zeroPos = true;
+			if(i+1 == left.segments.length) point.isLastPoint = true;
+			Robot.driveTrain.FrontRight.pushMotionProfileTrajectory(point);
+		}
+	}
+
+}
