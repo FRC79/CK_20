c60e43271e003cf8d7ddc059e955d44d0091f3fc
Noah <xlash19o13@gmail.com>
2017-02-11 14:39:43 -0500

    Format/OCD things

--- src/org/usfirst/frc/team79/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team79/robot/subsystems/Shooter.java
@@ -5,7 +5,6 @@ import org.usfirst.frc.team79.robot.RobotMap;
 import com.ctre.CANTalon;
 import com.ctre.CANTalon.FeedbackDevice;
 
-
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -13,19 +12,18 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Shooter extends Subsystem {
 
-		public CANTalon shooterWheel = new CANTalon(RobotMap.SHOOTER_MOTOR_1);
-		public CANTalon shooterWheelSlave = new CANTalon(RobotMap.SHOOTER_MOTOR_2);
+	public CANTalon shooterWheel = new CANTalon(RobotMap.SHOOTER_MOTOR_1);
+	public CANTalon shooterWheelSlave = new CANTalon(RobotMap.SHOOTER_MOTOR_2);
 
-    public Shooter() {
-      shooterWheelSlave.changeControlMode(CANTalon.TalonControlMode.Follower);
-      shooterWheelSlave.set(shooterWheel.getDeviceID());
-      shooterWheel.setFeedbackDevice(FeedbackDevice.QuadEncoder);
-      shooterWheel.configEncoderCodesPerRev(RobotMap.SHOOTER_TICKS_PER_REV);
+	public Shooter() {
+		shooterWheelSlave.changeControlMode(CANTalon.TalonControlMode.Follower);
+		shooterWheelSlave.set(shooterWheel.getDeviceID());
+		shooterWheel.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+		shooterWheel.configEncoderCodesPerRev(RobotMap.SHOOTER_TICKS_PER_REV);
+	}
 
-    }
+	public void initDefaultCommand() {
 
-    public void initDefaultCommand() {
-
-    }
+	}
 
 }
