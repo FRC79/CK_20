1b50bd2d415fd49c4e6d509b2919029f54b303ed
Noah Gil <xlash19o13@gmail.com>
2017-02-13 18:57:43 -0500

    Shifts motor based on given state

---
+++ src/org/usfirst/frc/team79/robot/utilities/ShiftState.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team79.robot.utilities;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
+public enum ShiftState {
+	
+	SHOOTER("Shooter", Value.kForward),
+	CLIMBER("Climber", Value.kReverse);
+	
+	public final String name;
+	public final Value value;
+	
+	private ShiftState(String name, Value value){
+		this.name = name;
+		this.value = value;
+	}
+	
+}
