1121f25be75a44e91904c8ec5f82c2957e302bc6
Noah Gil <xlash19o13@gmail.com>
2017-02-13 19:46:24 -0500

    Adds pitch controller

--- src/org/usfirst/frc/team79/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team79/robot/subsystems/Shooter.java
@@ -8,6 +8,7 @@ import com.ctre.CANTalon;
 import com.ctre.CANTalon.FeedbackDevice;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -18,6 +19,7 @@ public class Shooter extends Subsystem {
 	public CANTalon shooterWheel = new CANTalon(RobotMap.SHOOTER_MOTOR_1);
 	public CANTalon shooterWheelSlave = new CANTalon(RobotMap.SHOOTER_MOTOR_2);
 	public DoubleSolenoid shift = new DoubleSolenoid(RobotMap.SHIFT_SHOOTER, RobotMap.SHIFT_CLIMBER);
+	public Solenoid pitchController = new Solenoid(RobotMap.PITCH_CONTROLLER);
 
 	public Shooter() {
 		shooterWheelSlave.changeControlMode(CANTalon.TalonControlMode.Follower);
