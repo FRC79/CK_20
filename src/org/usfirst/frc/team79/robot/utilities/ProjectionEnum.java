9b7bc8905e00c38cb44211efef4a1d35368d4d22
Noah Gil <xlash19o13@gmail.com>
2017-02-14 19:02:59 -0500

    Enum for keeping track of projection angle

---
+++ src/org/usfirst/frc/team79/robot/utilities/ProjectionEnum.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team79.robot.utilities;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
+public enum ProjectionEnum {
+	
+	UP(30, Value.kForward),
+	DOWN(10, Value.kReverse);
+	
+	public final double angle;
+	public final Value value;
+	
+	ProjectionEnum(double angle){
+		this.angle = angle;
+		this.value = value;
+	}
+	
+	public static ProjectionEnum getByValue(Value value){
+		return value==Value.kForward ? UP : value==Value.kReverse ? DOWN : null;
+	}
+
+}
