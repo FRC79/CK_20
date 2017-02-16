092cd3ee6dd663a8196ba5c675999c84b629578a
Noah Gil <xlash19o13@gmail.com>
2017-02-15 19:14:14 -0500

    Will activate the shooter with the caculated speed

--- src/org/usfirst/frc/team79/robot/commands/auton/AutoShoot.java
+++ src/org/usfirst/frc/team79/robot/commands/auton/AutoShoot.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.usfirst.frc.team79.robot.Robot;
 import org.usfirst.frc.team79.robot.RobotMap;
+import org.usfirst.frc.team79.robot.commands.StartShooter;
 import org.usfirst.frc.team79.robot.utilities.ProjectionEnum;
 
 import edu.wpi.first.wpilibj.command.Command;
@@ -15,18 +16,28 @@ public class AutoShoot extends CommandGroup{
 	
 	public AutoShoot(){
 		add(new AlignShooter());
+		add(new StartShooter(0));
 	}
 	
 	public void initialize(){
+		double projection = Math.toRadians(ProjectionEnum.DOWN.angle);
+		double target = 0;
+		if((target=calculateVelocity(projection))>RobotMap.SHOOTER_MAX_VELOCITY){
+			target = calculateVelocity(ProjectionEnum.UP.angle);
+		}
+		((StartShooter)commands.get(1)).speed = target;
+		System.out.println("Shooter set to " + target + " RPM");
+	}
+	
+	public double calculateVelocity(double projection){
 		double distance = ProcessGripData.getDistance();
-		double projection = Math.toRadians(ProjectionEnum.getByValue(null).angle); //The solenoid for setting the projection is in button-mappings.
 		double ballVelocity = distance/(Math.cos(projection)*(Math.sqrt((RobotMap.BOILER_HEIGHT-RobotMap.CAMERA_HEIGHT-distance*Math.tan(projection))/(4.905))));
-		double shooterTargetVelocity = ballVelocity / RobotMap.SHOOTER_WHEEL_RADIUS / (2*Math.PI) * 60; //RPM
-		if(shooterTargetVelocity > RobotMap.SHOOTER_MAX_VELOCITY); //Change angle and try again.
+		return ballVelocity / RobotMap.SHOOTER_WHEEL_RADIUS / (2*Math.PI) * 60; // ft/s to RPM
 	}
 	
 	public void add(Command command){
 		this.addSequential(command);
+		commands.add(command);
 	}
 
 }
