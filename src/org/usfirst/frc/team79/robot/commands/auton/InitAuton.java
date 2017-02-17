91985b73d833e88994fad14422e0d649da2ce2c9
Noah Gil <xlash19o13@gmail.com>
2017-02-16 19:53:55 -0500

    Test waypoints

--- src/org/usfirst/frc/team79/robot/commands/auton/InitAuton.java
+++ src/org/usfirst/frc/team79/robot/commands/auton/InitAuton.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class InitAuton extends CommandGroup{
 	
 	public InitAuton(){
-		GenerateMotionProfile.generate(new FWaypoint(0,0,0), new FWaypoint(10,-1,0));
+		GenerateMotionProfile.generate(new FWaypoint(0,0,0), new FWaypoint(3,1,60), new FWaypoint(8,2,120), new FWaypoint(10, 3, 180));
 		this.addSequential(new ExecuteMotionProfile());
 	}
 
