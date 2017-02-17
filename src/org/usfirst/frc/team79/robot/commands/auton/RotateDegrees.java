70b4f1f28da4c018a0f0717ef791d8b2d917168c
Noah Gil <xlash19o13@gmail.com>
2017-02-17 14:55:22 -0500

    Gets grip heading in constructor

--- src/org/usfirst/frc/team79/robot/commands/auton/RotateDegrees.java
+++ src/org/usfirst/frc/team79/robot/commands/auton/RotateDegrees.java
@@ -1,11 +1,13 @@
 package org.usfirst.frc.team79.robot.commands.auton;
 
+import org.usfirst.frc.team79.robot.OI;
 import org.usfirst.frc.team79.robot.Robot;
 import org.usfirst.frc.team79.robot.utilities.GyroPIDOutput;
 import org.usfirst.frc.team79.robot.utilities.GyroPIDSource;
 
 import com.ctre.CANTalon.TalonControlMode;
 
+import edu.wpi.first.wpilibj.Joystick.AxisType;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.command.Command;
@@ -15,6 +17,7 @@ public class RotateDegrees extends Command {
 
 	double degrees;
 	public PIDController gyroPID;
+	private boolean grip;
 	
 	/**
 	 * Rotates based off the angle given. Positive = right.
@@ -22,13 +25,17 @@ public class RotateDegrees extends Command {
 	 */
 	public RotateDegrees(double degrees){
 		requires(Robot.driveTrain);
-		Robot.driveTrain.gyro.zeroYaw(); //Temporary for testing
 		this.degrees = degrees + Robot.driveTrain.getGyroAngle();
 		gyroPID = new PIDController(0, 0, 0, new GyroPIDSource(), new GyroPIDOutput());
-		}
+	}
+	
+	public RotateDegrees(){
+		this.grip = true;
+		gyroPID = new PIDController(0, 0, 0, new GyroPIDSource(), new GyroPIDOutput());
+	}
 	
 	protected void initialize(){
-		System.out.println("Initializing command");
+		if(grip) degrees = SmartDashboard.getNumber("Heading to Boiler", 0);
 		gyroPID.setPID(SmartDashboard.getNumber("Turn P", 0), SmartDashboard.getNumber("Turn I", 0), SmartDashboard.getNumber("Turn D", 0));
 		gyroPID.setContinuous();
 		gyroPID.setAbsoluteTolerance(0.9);
@@ -43,7 +50,10 @@ public class RotateDegrees extends Command {
 	}
 
 	protected boolean isFinished() {
-		return gyroPID.onTarget();
+		boolean flag;
+		if(grip) flag = Robot.oi.throttleStick.getY()!=0 || Robot.oi.throttleStick.getX()!=0;
+		flag = gyroPID.onTarget();
+		return flag;
 	}
 
 	protected void end() {
