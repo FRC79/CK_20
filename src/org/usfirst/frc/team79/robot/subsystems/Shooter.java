bcfcb93cf30a57d137fdde11c51efc59e1491323
Noah Gil <xlash19o13@gmail.com>
2017-02-17 15:28:47 -0500

    Changed name of solenoid to better name

--- src/org/usfirst/frc/team79/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team79/robot/subsystems/Shooter.java
@@ -18,7 +18,7 @@ public class Shooter extends Subsystem {
 	public CANTalon shooterWheel = new CANTalon(RobotMap.SHOOTER_MOTOR_1);
 	public CANTalon shooterWheelSlave = new CANTalon(RobotMap.SHOOTER_MOTOR_2);
 	public DoubleSolenoid shift = new DoubleSolenoid(RobotMap.SHIFT_SHOOTER, RobotMap.SHIFT_CLIMBER);
-	public Solenoid pitchController = new Solenoid(RobotMap.PITCH_CONTROLLER);
+	public Solenoid hoodController = new Solenoid(RobotMap.PITCH_CONTROLLER);
 
 	public Shooter() {
 		shooterWheelSlave.changeControlMode(CANTalon.TalonControlMode.Follower);
