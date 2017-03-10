dcabace9684fb2abc2ab07add575dfec2542b3ac
Krunch-79 <Krunch-79@Krunch-79-PC>
2017-03-10 07:35:32 -0800

    Because PID is trash

---
+++ src/org/usfirst/frc/team79/robot/commands/auton/RotateTime.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team79.robot.commands.auton;
+
+import org.usfirst.frc.team79.robot.Robot;
+
+import com.ctre.CANTalon.TalonControlMode;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class RotateTime extends Command{
+	
+	public double invert;
+	public Timer timer;
+	public double time;
+	public double speed;
+	
+	public RotateTime(boolean invert, double speed, double time){
+		this.invert = invert ? -1 : 1;
+		this.speed = speed;
+		this.time = time;
+	}
+	
+	public void initialize(){
+		Robot.driveTrain.FrontLeft.changeControlMode(TalonControlMode.PercentVbus);
+		Robot.driveTrain.FrontRight.changeControlMode(TalonControlMode.PercentVbus);
+		timer = new Timer();
+		timer.start();
+	}
+	
+	public void execute(){
+		System.out.println(speed*invert);
+		Robot.driveTrain.FrontLeft.set(-speed*invert);
+		Robot.driveTrain.FrontRight.set(speed*invert);
+	}
+	
+	@Override
+	protected boolean isFinished() {
+		return timer.hasPeriodPassed(time);
+	}
+
+}
