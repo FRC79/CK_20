a52bad8b45b07356623eb8dda3e3265d48082b35
Krunch-79 <Krunch-79@Krunch-79-PC>
2017-03-10 07:36:22 -0800

    Fixed displacement issue

--- src/org/usfirst/frc/team79/robot/commands/auton/RotateDegrees.java
+++ src/org/usfirst/frc/team79/robot/commands/auton/RotateDegrees.java
@@ -14,13 +14,13 @@ import org.usfirst.frc.team79.robot.utilities.GyroPIDOutput;
 import org.usfirst.frc.team79.robot.utilities.GyroPIDSource;
 
 public class RotateDegrees extends Command {
-	double degrees;
+	double deltaDegrees;
 	public PIDController gyroPID;
 	private boolean grip;
 
 	public RotateDegrees(double degrees) {
 		requires(Robot.driveTrain);
-		this.degrees = (degrees + Robot.driveTrain.getGyroAngle());
+		this.deltaDegrees = (degrees + Robot.driveTrain.getGyroAngle());
 		this.gyroPID = new PIDController(0.0D, 0.0D, 0.0D, new GyroPIDSource(), new GyroPIDOutput(), 0.01D);
 	}
 
@@ -30,25 +30,29 @@ public class RotateDegrees extends Command {
 	}
 
 	protected void initialize() {
+		Robot.driveTrain.FrontLeft.enableBrakeMode(true);
+		Robot.driveTrain.FrontRight.enableBrakeMode(true);
+		double degrees = 0;
 		if (this.grip) {
-			this.degrees = (ProcessGripData.getHeading() + Robot.driveTrain.getGyroAngle());
-		}
+			degrees = (ProcessGripData.getHeading() + Robot.driveTrain.getGyroAngle());
+		}else degrees = deltaDegrees + Robot.driveTrain.getGyroAngle();
 		this.gyroPID.setPID(SmartDashboard.getNumber("Turn P", 0.0D), SmartDashboard.getNumber("Turn I", 0.0D),
 				SmartDashboard.getNumber("Turn D", 0.0D));
 		this.gyroPID.setContinuous();
 		this.gyroPID.setAbsoluteTolerance(0.25D);
 		Robot.driveTrain.FrontLeft.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
 		Robot.driveTrain.FrontRight.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
-		this.gyroPID.setSetpoint(this.degrees);
+		this.gyroPID.setSetpoint(degrees);
 		this.gyroPID.enable();
-		System.out.println("Running RotateDegrees " + this.degrees);
+		System.out.println("Running RotateDegrees " + degrees);
 	}
 
 	protected void execute() {
+		
 	}
 
 	protected boolean isFinished() {
-		boolean flag = this.gyroPID.onTarget();
+		boolean flag = this.gyroPID.onTarget() && Math.abs(gyroPID.get()) < .09;
 		if ((this.grip) && (!flag)) {
 			flag = (Math.abs(Robot.oi.throttleStick.getY()) > 0.12D)
 					|| (Math.abs(Robot.oi.throttleStick.getX()) > 0.12D);
@@ -59,6 +63,8 @@ public class RotateDegrees extends Command {
 	protected void end() {
 		this.gyroPID.disable();
 		Robot.driveTrain.stop();
+		Robot.driveTrain.FrontLeft.enableBrakeMode(false);
+		Robot.driveTrain.FrontLeft.enableBrakeMode(false);
 	}
 
 	protected void interrupted() {
