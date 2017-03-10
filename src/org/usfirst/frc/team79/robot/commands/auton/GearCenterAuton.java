d58b124ff3befae6d015ff43dab452f05d4f6191
Krunch-79 <Krunch-79@Krunch-79-PC>
2017-03-10 05:45:58 -0800

    Center gear auton using MP

---
+++ src/org/usfirst/frc/team79/robot/commands/auton/GearCenterAuton.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team79.robot.commands.auton;
+
+import org.usfirst.frc.team79.robot.commands.GearDown;
+import org.usfirst.frc.team79.robot.commands.GearUp;
+import org.usfirst.frc.team79.robot.commands.WaitForIt;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class GearCenterAuton extends CommandGroup{
+	
+	public GearCenterAuton(){
+		this.addSequential(new ExecuteMotionProfile("GearSidePart1"));
+		this.addSequential(new WaitForIt(.2));
+		this.addSequential(new GearDown());
+		this.addSequential(new WaitForIt(.2));
+		this.addSequential(new ExecuteMotionProfile("GearSidePart3"));
+		this.addSequential(new GearUp());
+	}
+
+}
