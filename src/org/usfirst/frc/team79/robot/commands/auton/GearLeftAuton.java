ceafca5362db61536a2f577478871181944059aa
Krunch-79 <Krunch-79@Krunch-79-PC>
2017-03-10 23:54:45 -0800

    Moves forward for a shorter amount of time

--- src/org/usfirst/frc/team79/robot/commands/auton/GearLeftAuton.java
+++ src/org/usfirst/frc/team79/robot/commands/auton/GearLeftAuton.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class GearLeftAuton extends CommandGroup{
 	
 	public GearLeftAuton(){
-		this.addSequential(new DriveTime(-0.8D, 1.0D));
+		this.addSequential(new DriveTime(-0.8D, 0.965D));
 		this.addSequential(new WaitForIt(.8));
 		this.addSequential(new RotateTime(false, 0.5, 0.45));
 		this.addSequential(new WaitForIt(.8));
