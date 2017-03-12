0889d0810fca07cbc2f081996074afecb2be81cd
Krunch-79 <Krunch-79@Krunch-79-PC>
2017-03-11 23:48:54 -0800

    Works fairly well

--- src/org/usfirst/frc/team79/robot/commands/auton/DriveAngle.java
+++ src/org/usfirst/frc/team79/robot/commands/auton/DriveAngle.java
@@ -11,29 +11,41 @@ import org.usfirst.frc.team79.robot.Robot;
 import org.usfirst.frc.team79.robot.subsystems.DriveTrain;
 
 public class DriveAngle extends PIDCommand {
+	
 	public Timer timer;
 	public double speed;
 	public double angle;
 	public double time;
+	public boolean useCurrent;
 
 	/**
-	 * Was I high when I made this?
-	 * @param speed
-	 * @param angle
-	 * @param time
+	 * Drive along a fixed angle for a certain time at a certain speed.
+	 * @param speed Percent
+	 * @param angle Degrees
+	 * @param time Seconds
 	 */
 	public DriveAngle(double speed, double angle, double time) {
-		super(0.1D, 0.0D, 0.0D);
+		super(0.1D, 0.0D, 0.0D, 0.001);
 		this.speed = speed;
 		this.angle = angle;
 		this.time = time;
 	}
+	
+	/**
+	 * Drive along the current gyro angle for a certain time at a certain speed.
+	 * @param speed Percent
+	 * @param time Seconds
+	 */
+	public DriveAngle(double speed, double time){
+		this(speed, 0, time);
+		this.useCurrent = true;
+	}
 
 	public void initialize() {
+		if(useCurrent) angle = Robot.driveTrain.getGyroAngle();
 		Robot.driveTrain.FrontLeft.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
 		Robot.driveTrain.FrontRight.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
-		getPIDController().setPID(SmartDashboard.getNumber("Turn P", 0.0D), SmartDashboard.getNumber("Turn I", 0.0D),
-				SmartDashboard.getNumber("Turn D", 0.0D));
+		getPIDController().setPID(0.006, 0, 0);
 		this.timer = new Timer();
 		this.timer.start();
 	}
@@ -53,6 +65,10 @@ public class DriveAngle extends PIDCommand {
 	protected void usePIDOutput(double output) {
 		double left = output > 0.0D ? this.speed + output : this.speed;
 		double right = output < 0.0D ? this.speed - output : this.speed;
+		left = -left;
+		right = -right;
+		SmartDashboard.putNumber("PID output", output);
+		System.out.println(left + " " + right);
 		Robot.driveTrain.FrontLeft.set(left);
 		Robot.driveTrain.FrontRight.set(right);
 	}
